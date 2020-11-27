8a3106cd04abc151aec6181a398664004ed5e10a
Alekseyesss <alewa86@gmail.com>
2020-11-27 14:32:31 +0200

    added timer coding in native JS

--- js/script.js
+++ js/script.js
@@ -129,7 +129,56 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-  });
+  }); // Timer
+
+  const deadline = '2020-12-01';
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(t / (1000 * 60 * 60) % 24),
+          minutes = Math.floor(t / (1000 * 60) % 60),
+          seconds = Math.floor(t / 1000 % 60);
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+
+    function getZero(num) {
+      if (num >= 0 && num < 10) {
+        return `0${num}`;
+      } else {
+        return num;
+      }
+    }
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerText = getZero(t.days);
+      hours.innerText = getZero(t.hours);
+      minutes.innerText = getZero(t.minutes);
+      seconds.innerText = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
 });
 
 /***/ })
